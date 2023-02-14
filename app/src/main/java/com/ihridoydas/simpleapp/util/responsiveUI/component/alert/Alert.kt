2efff462e77aa55b93dc74836597029a1b2862da
ihridoydas <hridoycseboy@gmail.com>
2023-02-14 09:20:51 +0900

    Implement Alert with viewModel

---
+++ app/src/main/java/com/ihridoydas/simpleapp/util/responsiveUI/component/alert/Alert.kt
@@ -0,0 +1,206 @@
+package com.ihridoydas.simpleapp.util.responsiveUI.component.alert
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.AlertDialog
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.PlatformTextStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.window.DialogProperties
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.ihridoydas.simpleapp.util.responsiveUI.WindowType
+import com.ihridoydas.simpleapp.util.responsiveUI.dpToSp
+import com.ihridoydas.simpleapp.util.responsiveUI.rememberWindowSize
+
+
+/**
+ * Created by DavidA on 2022/07/06.
+ * Copyright © 2022 Cognivision inc. All rights reserved.
+ */
+
+/**
+ * Alert modal with title, text and buttons
+ * @see AlertViewModel.show to show the alert
+ */
+@Composable
+fun Alert(isPreview: Boolean = false) {
+    if (isPreview) return
+    val viewModel: AlertViewModel = viewModel()
+    // window Size
+    val window = rememberWindowSize()
+
+    if (viewModel.alertInstance.isVisible && viewModel.title.isNotEmpty()) {
+        AlertDialog(
+            modifier = Modifier
+                .padding(8.dp)
+                .clip(RoundedCornerShape(5.dp))
+                .background(MaterialTheme.colorScheme.background),
+            backgroundColor = MaterialTheme.colorScheme.background,
+            properties = DialogProperties(
+                dismissOnClickOutside = false,
+                dismissOnBackPress = true,
+            ),
+            onDismissRequest = {
+                viewModel.backAction?.let {
+                    viewModel.callDismissAction()
+                }
+            },
+            title ={
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    if (viewModel.title.isNotEmpty()) {
+                            Text(
+                                text = viewModel.title,
+                                textAlign = TextAlign.Center,
+                                color = Color.Black,
+                                fontWeight = FontWeight.SemiBold,
+                                style = TextStyle(
+                                    lineHeight = 1.5.em,
+                                    platformStyle = PlatformTextStyle(
+                                        includeFontPadding = false
+                                    ),
+                                    fontSize = when (window.width) {
+                                        WindowType.Normal -> dpToSp(18.dp)
+                                        else -> dpToSp(15.dp)
+                                    },
+                                    lineHeightStyle = LineHeightStyle(
+                                        alignment = LineHeightStyle.Alignment.Center,
+                                        trim = LineHeightStyle.Trim.None
+                                    )
+                            ))
+
+                    } else {
+                        null
+                    }
+                }
+
+            },
+
+            text ={
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    if (viewModel.text.isNotEmpty()) {
+
+                            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                                Text(
+                                    text = viewModel.text,
+                                    textAlign = TextAlign.Center,
+                                    color = Color.Black,
+                                    maxLines = Int.MAX_VALUE,
+                                    style = TextStyle(
+                                        lineHeight = 1.5.em,
+                                        platformStyle = PlatformTextStyle(
+                                            includeFontPadding = false
+                                        ),
+                                        fontWeight = FontWeight.Medium,
+                                        fontSize = when (window.width) {
+                                            WindowType.Normal -> dpToSp(14.dp)
+                                            else -> dpToSp(12.dp)
+                                        },
+                                        lineHeightStyle = LineHeightStyle(
+                                            alignment = LineHeightStyle.Alignment.Center,
+                                            trim = LineHeightStyle.Trim.None
+                                        )
+                                    ),
+                                    overflow = TextOverflow.Visible,
+                                )
+                        }
+                    } else {
+                        null
+                    }
+                }
+            },
+
+            buttons = {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Divider(Modifier.background(Color.Black))
+                    if (viewModel.button1Text.isNotEmpty()) {
+                        Button(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            onClick = {
+                                viewModel.button1Action()
+                            },
+                            colors = ButtonDefaults.textButtonColors(
+                                containerColor = Color.Transparent,
+                                contentColor = Color.Blue,
+                                disabledContentColor = Color.Transparent
+                            )
+                        ) {
+                            Text(
+                                viewModel.button1Text,
+                                fontSize = when (window.height) {
+                                    WindowType.Normal -> dpToSp(18.dp)
+                                    else -> dpToSp(16.dp)
+                                }
+                            )
+                        }
+                    }
+                    if (viewModel.button2Text.isNotEmpty()) {
+                        Divider(Modifier.background(Color.Black))
+                        Button(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            onClick = {
+                                viewModel.button2Action()
+                            },
+                            colors = ButtonDefaults.textButtonColors(
+                                containerColor = Color.Transparent,
+                                contentColor = Color.Blue,
+                                disabledContentColor = Color.Transparent
+                            )
+                        ) {
+                            Text(
+                                viewModel.button2Text,
+                                fontSize = when (window.height) {
+                                    WindowType.Normal -> dpToSp(18.dp)
+                                    else -> dpToSp(16.dp)
+                                }
+                            )
+                        }
+                    }
+                    if (viewModel.button3Text.isNotEmpty()) {
+                        Divider(Modifier.background(Color.Black))
+                        Button(
+                            modifier = Modifier.fillMaxWidth(),
+                            onClick = {
+                                viewModel.button3Action()
+                            },
+                            colors = ButtonDefaults.textButtonColors(
+                                containerColor = Color.Transparent,
+                                contentColor = Color.Blue,
+                                disabledContentColor = Color.Transparent
+                            )
+                        ) {
+                            Text(
+                                viewModel.button3Text,
+                                fontSize = when (window.height) {
+                                    WindowType.Normal -> dpToSp(18.dp)
+                                    else -> dpToSp(16.dp)
+                                }
+                            )
+                        }
+                    }
+                }
+            }
+        )
+    }
+}
---
+++ app/src/main/java/com/ihridoydas/simpleapp/util/responsiveUI/component/alert/AlertViewModel.kt
@@ -0,0 +1,129 @@
+
+package com.ihridoydas.simpleapp.util.responsiveUI.component.alert
+
+import androidx.compose.runtime.*
+import androidx.lifecycle.ViewModel
+import com.ihridoydas.simpleapp.util.common.EmptyParamsNoReturnFun
+import com.ihridoydas.simpleapp.util.common.EmptyParamsNoReturnNullableFun
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+enum class ButtonActions {
+    Button1, Button2, Button3
+}
+
+/**
+ * ViewModel for Alert
+ * UI-> Alert()
+ */
+@HiltViewModel
+class AlertViewModel @Inject constructor() : ViewModel() {
+
+    var title by mutableStateOf("")
+    var text by mutableStateOf("")
+    var button1Text by mutableStateOf("")
+    var button1Action: EmptyParamsNoReturnFun = {}
+    var button2Text by mutableStateOf("")
+    var button2Action: EmptyParamsNoReturnFun = {}
+    var button3Text by mutableStateOf("")
+    var button3Action: EmptyParamsNoReturnFun = {}
+
+    /** Called before showing dialog */
+    private var showHook: EmptyParamsNoReturnNullableFun = null
+    /** Called before hiding dialog */
+    private var hideHook: EmptyParamsNoReturnNullableFun = null
+    /** Button Action to call on Android back button press*/
+    var backAction: ButtonActions? = null
+
+    /**
+     * Show alert, button1 is not optional, other buttons optional
+     * @param title Title
+     * @param text Text shown under title
+     * @param button1Text Text to show on button1
+     * @param button1Action Lambda Action to perform when clicking button 1
+     * @param button2Text Text to show on button2
+     * @param button2Action Lambda Action to perform when clicking button 2
+     * @param button3Text Text to show on button3
+     * @param button3Action Lambda Action to perform when clicking button 3
+     * @param backAction Existing button action to call when back button pressed
+     */
+    fun show(
+        title: String,
+        text: String,
+        button1Text: String,
+        button1Action: () -> Unit,
+        button2Text: String = "",
+        button2Action: () -> Unit = {},
+        button3Text: String = "",
+        button3Action: () -> Unit = {},
+        backAction: ButtonActions? = null
+    ) {
+        showHook?.let { it() }
+        this.title = title
+        this.text = text
+        this.button1Text = button1Text
+        this.button1Action = button1Action
+        this.button2Text = button2Text
+        this.button2Action = button2Action
+        this.button3Text = button3Text
+        this.button3Action = button3Action
+        this.backAction = backAction
+    }
+
+    /** * Hide the alert */
+    fun hide(runHook: Boolean = true) {
+        hideHook?.let { it() }
+    }
+
+    fun callDismissAction() {
+        when (backAction) {
+            ButtonActions.Button1 -> button1Action.invoke()
+            ButtonActions.Button2 -> button2Action.invoke()
+            ButtonActions.Button3 -> button3Action.invoke()
+            else -> {}
+        }
+    }
+
+    /**
+     *  UI→　NumberPlateAlertDialog()
+     */
+    fun showConfirmationDialog(
+        title: String,
+        text: String,
+        button1Text: String,
+        button1Action: () -> Unit,
+        button2Text: String = "",
+        button2Action: () -> Unit = {},
+        button3Text: String = "",
+        button3Action: () -> Unit = {},
+        backAction: ButtonActions? = null
+    ){
+        showHook?.let { it() }
+        this.title = title
+        this.text = text
+        this.button1Text = button1Text
+        this.button1Action = button1Action
+        this.button2Text = button2Text
+        this.button2Action = button2Action
+        this.button3Text = button3Text
+        this.button3Action = button3Action
+        this.backAction = backAction
+    }
+
+    /**
+     * タイトルと一つボタンだけ表示の場合
+     * UI→　SingleButtonAlertDialog()
+     */
+    fun showDialogWithTitle(
+        title: String,
+        button1Text: String,
+        button1Action: () -> Unit,
+        backAction: ButtonActions? = null
+    ){
+        showHook?.let { it() }
+        this.title = title
+        this.button1Text = button1Text
+        this.button1Action = button1Action
+        this.backAction = backAction
+    }
+}
