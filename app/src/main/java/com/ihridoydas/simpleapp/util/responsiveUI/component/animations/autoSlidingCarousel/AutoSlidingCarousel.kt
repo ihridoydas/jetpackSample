1645bd3fed98cfa86bbd29f98efde4dae03f5bf2
ihridoydas <hridoycseboy@gmail.com>
2023-04-09 14:50:42 +0900

    Added slider carousel animation jetpack compose

---
+++ app/src/main/java/com/ihridoydas/simpleapp/util/responsiveUI/component/animations/autoSlidingCarousel/AutoSlidingCarousel.kt
@@ -0,0 +1,152 @@
+package com.ihridoydas.simpleapp.util.responsiveUI.component.animations.autoSlidingCarousel
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.collectIsDraggedAsState
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.PagerState
+import kotlinx.coroutines.delay
+import com.ihridoydas.simpleapp.R
+
+@Composable
+fun IndicatorDot(
+    modifier: Modifier = Modifier,
+    size: Dp,
+    color: Color
+) {
+    Box(
+        modifier = modifier
+            .size(size)
+            .clip(CircleShape)
+            .background(color)
+    )
+}
+
+@Composable
+fun DotsIndicator(
+    modifier: Modifier = Modifier,
+    totalDots: Int,
+    selectedIndex: Int,
+    selectedColor: Color = Color.Yellow, /* IndicatorSelectedColor*/
+    unSelectedColor: Color = Color.Gray,  /* IndicatorUnselectedColor */
+    dotSize: Dp
+) {
+    LazyRow(
+        modifier = modifier
+            .wrapContentWidth()
+            .wrapContentHeight()
+    ) {
+        items(totalDots) { index ->
+            IndicatorDot(
+                color = if (index == selectedIndex) selectedColor else unSelectedColor,
+                size = dotSize
+            )
+
+            if (index != totalDots - 1) {
+                Spacer(modifier = Modifier.padding(horizontal = 2.dp))
+            }
+        }
+    }
+}
+
+const val AUTO_SLIDE_DURATION = 9000L
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun AutoSlidingCarousel(
+    modifier: Modifier = Modifier,
+    autoSlideDuration: Long = AUTO_SLIDE_DURATION,
+    pagerState: PagerState = remember { PagerState() },
+    itemsCount: Int,
+    itemContent: @Composable (index: Int) -> Unit,
+) {
+    val isDragged by pagerState.interactionSource.collectIsDraggedAsState()
+
+    LaunchedEffect(pagerState.currentPage) {
+        delay(autoSlideDuration)
+        pagerState.animateScrollToPage((pagerState.currentPage + 1) % itemsCount)
+    }
+
+    Box(
+        modifier = modifier.fillMaxWidth(),
+    ) {
+        HorizontalPager(count = itemsCount, state = pagerState) { page ->
+            itemContent(page)
+        }
+
+        // you can remove the surface in case you don't want
+        // the transparant bacground
+        Surface(
+            modifier = Modifier
+                .padding(bottom = 8.dp)
+                .align(Alignment.BottomCenter),
+            shape = CircleShape,
+            color = Color.Black.copy(alpha = 0.5f)
+        ) {
+            DotsIndicator(
+                modifier = Modifier.padding(horizontal = 8.dp, vertical = 6.dp),
+                totalDots = itemsCount,
+                selectedIndex = if (isDragged) pagerState.currentPage else pagerState.targetPage,
+                dotSize = 8.dp
+            )
+        }
+    }
+}
+
+// Use case Demo
+@OptIn(ExperimentalPagerApi::class)
+@Preview
+@Composable
+fun AutoSlidingCarousel() {
+
+    val images = listOf(
+        "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+        "https://cdn.pixabay.com/photo/2023/01/23/21/11/apple-7739714_1280.jpg",
+        "https://cdn.pixabay.com/photo/2023/03/29/14/44/mushroom-7885658_1280.jpg",
+        "https://cdn.pixabay.com/photo/2014/04/14/20/11/pink-324175_1280.jpg"
+
+    )
+
+    Card(
+        modifier = Modifier.padding(16.dp),
+        shape = RoundedCornerShape(16.dp),
+    ) {
+        AutoSlidingCarousel(
+            itemsCount = images.size,
+            itemContent = { index ->
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(images[index])
+                        .crossfade(true)
+                        .build(),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.height(200.dp),
+                )
+            }
+        )
+    }
+
+}
---
+++ app/src/main/java/com/ihridoydas/simpleapp/util/responsiveUI/component/animations/downloadableAnimationCircle/Utils.kt
@@ -0,0 +1,28 @@
+package com.ihridoydas.simpleapp.util.responsiveUI.component.animations.downloadableAnimationCircle
+
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import kotlin.math.max
+import kotlin.math.sin
+
+fun getSinusoidalPoints(
+    size: Size,
+    startOffset: Float = 0f,
+    endOffset: Float = 0f,
+    startPercentage: Float = 0f
+): MutableList<Offset> {
+    val points = mutableListOf<Offset>()
+    val verticalCenter = size.height / 2
+
+    for (x in 0 until (size.width - startOffset - endOffset).toInt() step 10) {
+        val y = (sin((x + endOffset + startPercentage * size.width) * (2f * Math.PI / size.width)) * verticalCenter + verticalCenter).toFloat()
+        points.add(Offset(x.toFloat() + startOffset, y))
+    }
+
+    if (points.isEmpty() || endOffset > 0f) {
+        points.add(Offset(size.width - endOffset, size.height / 2))
+        points.add(Offset(size.width, size.height / 2))
+    }
+
+    return points
+}
