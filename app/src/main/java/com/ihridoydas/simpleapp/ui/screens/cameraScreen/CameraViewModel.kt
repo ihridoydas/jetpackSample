e6f883cdd96295cc17a6cb659b802e677d24e81f
ihridoydas <hridoycseboy@gmail.com>
2023-04-28 22:57:35 +0900

    ADD Camera Functionality and upgrade Gradle and other versions

---
+++ app/src/main/java/com/ihridoydas/simpleapp/ui/screens/cameraScreen/CameraViewModel.kt
@@ -0,0 +1,38 @@
+package com.ihridoydas.simpleapp.ui.screens.cameraScreen
+
+import android.content.Context
+import androidx.camera.view.PreviewView
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ihridoydas.simpleapp.data.repository.camera.CustomCameraRepo
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class CameraViewModel @Inject constructor(
+    private val repo: CustomCameraRepo
+):ViewModel() {
+
+    fun showCameraPreview(
+        previewView: PreviewView,
+        lifecycleOwner: LifecycleOwner
+    ){
+        viewModelScope.launch {
+            repo.showCameraPreview(
+                previewView,
+                lifecycleOwner
+            )
+        }
+    }
+
+    fun captureAndSave(context: Context){
+        viewModelScope.launch {
+            repo.captureAndSaveImage(context)
+        }
+    }
+
+
+}
