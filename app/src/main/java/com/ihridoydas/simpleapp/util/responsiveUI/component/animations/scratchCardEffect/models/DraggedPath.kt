43a9fad39282b8daa81549857a5c5c8b0161e54b
ihridoydas <hridoycseboy@gmail.com>
2023-05-05 21:54:03 +0900

    Implement ScratchCardEffect in Animations

---
+++ app/src/main/java/com/ihridoydas/simpleapp/util/responsiveUI/component/animations/scratchCardEffect/models/DraggedPath.kt
@@ -0,0 +1,8 @@
+package com.ihridoydas.simpleapp.util.responsiveUI.component.animations.scratchCardEffect.models
+
+import androidx.compose.ui.graphics.Path
+
+data class DraggedPath(
+    val path: Path,
+    val width: Float = 50f
+)
