6701de8db81207b8a9c85ff065fb986bcd201366
ihridoydas <hridoycseboy@gmail.com>
2023-05-07 14:03:00 +0900

    Notification Badge UI implement

--- app/src/main/java/com/ihridoydas/simpleapp/util/extensions/ModifierExtension.kt
+++ app/src/main/java/com/ihridoydas/simpleapp/util/extensions/ModifierExtension.kt
@@ -6,7 +6,11 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.Dp
 
 fun Modifier.conditional(condition : Boolean, modifier : Modifier.() -> Modifier) : Modifier {
     return if (condition) {
@@ -38,4 +42,21 @@ fun Modifier.clickableNoRipple(
         role = role,
         onClick = onClick
     )
+}
+
+//Icons And Image Badge Extension
+fun Modifier.badge(toggle:Boolean, radius: Dp, color : Color = Color.Red ,topPadding: Dp, rightPadding : Dp) : Modifier {
+
+    return this.drawWithContent {
+        //draw the image itself
+        this.drawContent()
+        if (toggle) {
+            //draw the badge over the image
+            this.drawCircle(
+                color,
+                radius.toPx(),
+                Offset(size.width - rightPadding.toPx(),topPadding.toPx())
+            )
+        }
+    }
 }
