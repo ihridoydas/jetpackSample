7f219eee56edf2f554141d13d061bad275dd7bfd
ihridoydas <hridoycseboy@gmail.com>
2023-08-22 14:22:00 +0900

    Fancy Clock Android Composible

--- app/src/main/java/com/ihridoydas/simpleapp/util/responsiveUI/component/animations/othersAnimation/UseCase.kt
+++ app/src/main/java/com/ihridoydas/simpleapp/util/responsiveUI/component/animations/othersAnimation/UseCase.kt
@@ -23,6 +23,7 @@ import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.PagerState
 import com.google.accompanist.pager.rememberPagerState
 import com.ihridoydas.simpleapp.ui.theme.ThemeColor
+import com.ihridoydas.simpleapp.util.responsiveUI.component.animations.composeFancyClock.FancyClock
 import com.ihridoydas.simpleapp.util.responsiveUI.component.animations.waveTimerAnimation.WavesTimerAnimation
 import com.ihridoydas.simpleapp.util.responsiveUI.component.indicator.ScribbleIndicator
 import com.ihridoydas.simpleapp.util.responsiveUI.component.indicator.ScribbleIndicatorScreen
@@ -33,7 +34,7 @@ import com.ihridoydas.simpleapp.util.responsiveUI.component.shape.ticketShape.Al
 val list = listOf("ArcRotation", "CircleOffset", "ClockLoading",
     "HeartAnimation","PacmanAnimation","ProgressAnimation","RotationDotAnimation",
     "RotationTwoDotAnimation","RotatingCircle","RotatingSquare","SquareFillLoaderAnimation",
-    "StepperAnimation","ThreeBounceAnimation","TwinCircleAnimation","WaveAnimation","WavesTimerAnimation","AllShape","UndoRedoAnimation","ScribbleIndicator")
+    "StepperAnimation","ThreeBounceAnimation","TwinCircleAnimation","WaveAnimation","WavesTimerAnimation","AllShape","UndoRedoAnimation","ScribbleIndicator","FancyClock")
 
 @Composable
 fun AnimationTabsContent(pagerState: PagerState) {
@@ -64,6 +65,7 @@ fun AnimationTabsContent(pagerState: PagerState) {
                 16 -> AllShape()
                 17 -> UndoRedoAnimation()
                 18 -> ScribbleIndicator()
+                19 -> FancyClock()
 
             }
 
