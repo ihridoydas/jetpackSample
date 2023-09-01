f7f452513b82ee2a6eec399342774a81bd6bcb43
ihridoydas <hridoycseboy@gmail.com>
2023-09-01 12:31:55 +0900

    Hot fix AR Ecommerce (pager library updated reasion)

--- app/src/main/java/com/ihridoydas/simpleapp/ar/arEcommerce/productdescription/presentation/ImageCarousel.kt
+++ app/src/main/java/com/ihridoydas/simpleapp/ar/arEcommerce/productdescription/presentation/ImageCarousel.kt
@@ -2,8 +2,12 @@ package com.ihridoydas.simpleapp.ar.arEcommerce.productdescription.presentation
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PageSize
+import androidx.compose.foundation.pager.PagerDefaults
+import androidx.compose.foundation.pager.PagerScope
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
@@ -20,19 +24,38 @@ import com.bumptech.glide.integration.compose.GlideImage
 @Composable
 fun ImageCarousel(images: List<String>) {
     val pageCount = images.size
-    val pagerState = rememberPagerState()
+    val pagerState = rememberPagerState(
+        initialPage = 0,
+        initialPageOffsetFraction = 0f
+    ) {
+        // provide pageCount
+        pageCount
+    }
     Box(modifier = Modifier.fillMaxHeight(0.3f)) {
+        // Use your favorite image loading library here!
         HorizontalPager(
+            modifier = Modifier,
             state = pagerState,
-            pageCount = pageCount
-        ) { page ->
-            // Use your favorite image loading library here!
-            GlideImage(
-                model = images[page],
-                contentScale = ContentScale.FillWidth,
-                contentDescription = "image_$page"
-            )
-        }
+            pageSpacing = 0.dp,
+            userScrollEnabled = true,
+            reverseLayout = false,
+            contentPadding = PaddingValues(0.dp),
+            beyondBoundsPageCount = 0,
+            pageSize = PageSize.Fill,
+            flingBehavior = PagerDefaults.flingBehavior(state = pagerState),
+            key = null,
+            pageNestedScrollConnection = PagerDefaults.pageNestedScrollConnection(
+                Orientation.Horizontal
+            ),
+            pageContent = {
+                // Use your favorite image loading library here!
+                GlideImage(
+                    model = images[it],
+                    contentScale = ContentScale.FillWidth,
+                    contentDescription = "image_$it"
+                )
+            }
+        )
         Row(
             Modifier
                 .height(30.dp)
