18a5e457839b2e8a78534b2f9c7214e09141c1af
ihridoydas <hridoycseboy@gmail.com>
2024-02-05 16:02:20 +0900

    Add Data Preview in Compose

---
+++ app/src/main/java/com/ihridoydas/simpleapp/features/preview/DataModels.kt
@@ -0,0 +1,46 @@
+package com.ihridoydas.simpleapp.features.preview
+
+import DataPreview
+import java.text.SimpleDateFormat
+import java.util.*
+
+@DataPreview
+data class PaymentHistoryItem(
+    val _id: String,
+    val amount: Int,
+    val currentBalance: Int,
+    val date: Long,
+    val fromUser: User,
+    val id: String,
+    val oldBalance: Int,
+    val toUser: User,
+    val status: String = ""
+) {
+    var dateString = ""
+        private set
+
+    fun generateDate() {
+        try {
+            val sdf = SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.US)
+            val netDate = Date(date)
+            dateString = sdf.format(netDate)
+        } catch (_: Exception) {
+        }
+    }
+}
+
+data class User(
+    val _id: String,
+    val fullName: String,
+    val hasProducts: Boolean,
+    val id: String,
+    val location: Location,
+    val name: String,
+    val surname: String,
+    val username: String
+)
+
+data class Location(
+    val lat: Int,
+    val long: Int,
+)
